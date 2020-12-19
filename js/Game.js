a6062e0a9d58b681eca45080396eae50c497a6fc
Selma <emmasmith1656@gmail.com>
2020-12-18 20:47:20 -0800

    happy birthday

--- js/Game.js
+++ js/Game.js
@@ -1,6 +1,6 @@
 class Game {
   constructor(){}
-  
+
   getState(){
     var gameStateRef  = database.ref('gameState');
     gameStateRef.on("value",function(data){
@@ -23,4 +23,30 @@ class Game {
       form.display();
     }
   }
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100)
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      var display_position = 130;
+      for(var plr in allPlayers){
+        if (plr === "player" + player.index)
+          fill("red")
+        else
+          fill("black");
+
+        display_position+=20;
+        textSize(15);
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=50
+      player.update();
+    }
+  }
 }
+
